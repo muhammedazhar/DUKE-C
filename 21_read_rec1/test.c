4e7c1ba283a2d6b41b01fb313234b4131b830806
Muhammed Azhar <iam.muhammedazhar@gmail.com>
2023-11-30 15:18:10 +0530

    Update 'test.c' file.

--- 21_read_rec1/test.c
+++ 21_read_rec1/test.c
@@ -2,27 +2,26 @@
 #include <stdlib.h>
 
 void printDigits(int x) {
-  if (x == 0) {
-    printf("0");
-  }
-  else if (x < 0) {
-    printf("-");
-    printDigits(-x);
-  }
-  else {
-    int a = x/10;
-    int b = x %10;
-    printf("a=%d, b=%d\n",a,b);
-    if (a != 0) {
-      printDigits(a);
+    if(x == 0) {
+        printf("0");
     }
-    printf("%d",b);
-  }
+    else if(x < 0) {
+        printf("-");
+        printDigits(-x);
+    }
+    else {
+        int a = x/10;
+        int b = x %10;
+        printf("a=%d, b=%d\n",a,b);
+        if(a != 0) {
+            printDigits(a);
+        }
+        printf("%d",b);
+    }
 }
 
-
 int main(void) {
-  printDigits(297);
-  printf("\n");
-  return EXIT_SUCCESS;
+    printDigits(297);
+    printf("\n");
+    return EXIT_SUCCESS;
 }
