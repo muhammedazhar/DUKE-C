4c538c8f1e7edaf5b95982fb6b2a0f8af51416f8
Muhammed Azhar <iam.muhammedazhar@gmail.com>
2023-11-30 16:03:01 +0530

    Update 'outname_test.c', 'outname.c' and 'outname.h' files.

--- 29_outname/outname_test.c
+++ 29_outname/outname_test.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #define NUM_TESTS 3
 int main(void) {
-  char * testNames[NUM_TESTS] = {"input.txt",
-				 "anotherTestFileName.txt",
-				 "somethingelse"};
-  
-  for (int i = 0; i < NUM_TESTS; i++) {
-    char * outName = computeOutputFileName(testNames[i]);
-    printf("'%s' => '%s'\n", testNames[i], outName);
-    free(outName);
-  }
-  return EXIT_SUCCESS;
+    char * testNames[NUM_TESTS] = {
+        "input.txt",
+        "anotherTestFileName.txt",
+        "somethingelse"
+    };
+    for (int i = 0; i < NUM_TESTS; i++) {
+        char * outName = computeOutputFileName(testNames[i]);
+        printf("'%s' => '%s'\n", testNames[i], outName);
+        free(outName);
+    }
+    return EXIT_SUCCESS;
 }
